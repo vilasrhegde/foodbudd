e2a759db2cdadbfaea9c48c9b6bc6ae310663128
Vilas Hegde <85540091+vilasrhegde@users.noreply.github.com>
2021-10-19 00:24:31 +0530

    Add files via upload
    feature update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React,{useEffect,useState} from "react";
 import Recipe from "./recipe";
 import './App.css';
 
+
 // from chrome
 // const http = require('http');
 // const port = process.env.PORT || 3000
@@ -22,25 +23,37 @@ const App = () =>{
 
   const APP_ID="2476cbe4";
   const APP_KEY="b19127c8bf8749ab8cc49ad126ccbf6d";
+
   const[counter,setCounter]=useState(0);
 
   const [recipes,setRecipes] = useState([]);
   const [search,setSearch] = useState("");
 
-  const [query,setQuery] = useState('recipe');
+  const [query,setQuery] = useState("");
+  const [healthLabels, sethealthLabel] = useState("vegan")
 
+  var url = `https://api.edamam.com/search?&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${healthLabels}`;
+  // async function getRecipes(){
+  //   var result = await Axios.get(url);
+  //   console.log(result.data);
+  // }
   useEffect(()=>{
        getRecipes();
        },[query]);
 
   const getRecipes = async ()=>{
-    const response = await fetch(`https://api.edamam.com/search?&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${healthLabels}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data);
+
   };
+
+  
 
   const updateSearch = e =>{
     setSearch(e.target.value);
+    console.log();
   };
 
   const getSearch = e =>{
@@ -48,23 +61,42 @@ const App = () =>{
     setQuery(search);
   };
 
+ 
+
   return(
     <div  className="App">
     <br/>
     <div id="pg1"></div>
     <div className="header">
     <center><a href="#pg1"><h1 className="appname">Foo<span>db</span>udd</h1></a></center>
-    <h2 className="fh1" onClick={()=>setCounter(counter+1)}>     Hello Foodies! {counter}</h2> 
+    {/* <h2 className="fh1" onClick={()=>setCounter(counter+1)}>     Hello Foodies! {counter}</h2>  */}
  
     </div>
    
 
     <form  onSubmit={getSearch} className="search-form">
       <input className="search-bar" placeholder="Find out more..." type="text" value={search} onChange={updateSearch}/>
+      <select className="app_healthLabels">
+        <option className="dropcontent"   onClick={()=>sethealthLabel("vegan")}>Vegan</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("paleo")}>Paleo</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("low-sugar")}>Low-Sugar</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("dairy-free")}>Dairy-free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("gluten-free")}>Gluten-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("wheat-free")}>Wheat-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("egg-free")}>Egg-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("peanut-free")}>Peanut-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("Tree-nut-free")}>Tree-nut--Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("soy-free")}>Soy-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("fish-free")}>Fish-Free</option>
+        <option className="dropcontent"   onClick={()=>sethealthLabel("shellfish-free")}>Shellfish-Free</option>
+
+
+      </select>
       <button className="search-button"
        type="submit"> 
         🔍                                               
       </button>
+     
     </form>
     <div  className="recipes">
     {recipes.map(recipe=>(
@@ -73,7 +105,18 @@ const App = () =>{
        title={recipe.recipe.label}
         calories={recipe.recipe.calories} 
         image={recipe.recipe.image}
-         ingredients={recipe.recipe.ingredients} />
+         ingredients={recipe.recipe.ingredients} 
+          healthLabels={recipe.recipe.healthLabels}
+          digest={recipe.recipe.digest}
+          dishType={recipe.recipe.dishType}
+          mealType={recipe.recipe.mealType}
+          cautions={recipe.recipe.cautions}
+          cuisineType={recipe.recipe.cuisineType}
+          // totalDaily={recipe.recipe.totalDaily}
+          url={recipe.recipe.url}
+          totalNutrients={recipe.recipe.totalNutrients}
+
+         />
     ))}
     </div>
    
--- src/recipe.js
+++ src/recipe.js
@@ -1,13 +1,17 @@
 import React from "react";
 import style from "./recipe.module.css";
 
-const Recipe =({title,calories,image,ingredients})=>{
+
+// digest,cuisineType,mealType,cautions
+const Recipe =({title,calories,image,ingredients,healthLabels,cautions,dishType,mealType,digest,cuisineType,totalDaily,url,totalNutrients})=>{
     return (
-        <div className={style.recipe}>
+        image.match(/\.(jpeg|jpg|gif|png)$/)
+
+       !=null&&( <div className={style.recipe} onClick={url} >
             <h1>{title}</h1>
-            <img className={style.image} src={image} alt="" />
+            <img className={style.image} src={image} alt="image" />
             <ol>
-            <h3>Ingredients:</h3>
+            <h3>How to make?</h3>
                 {ingredients.map(ingredients =>(
                  <li>
                      {ingredients.text}
@@ -15,8 +19,22 @@ const Recipe =({title,calories,image,ingredients})=>{
                  ))}
             </ol>   
             <p><b>Calories : </b>{calories}</p>
+            <p><b>Cautions: </b>{cautions}</p> 
+            <p><b>Dish Type: </b>{dishType}</p>
+            <p><b>Meal type: </b>{mealType}</p> 
+            <p><b>Cuisine type: </b>{cuisineType}</p>
+            {/* <p><b>Total Nutrients: </b>{totalNutrients}</p> */}
+            {/* <p><b>Total Daily: </b>{totalDaily}</p> */}
+            <button className="knowmore">
+            <a href={url} target="_blank">Know more</a>
+            </button>
+            {/* <p className="health"><b>Health label :</b>{healthLabels} </p> */}
+            {/* <p><b>Digest: </b>{digest}</p> */}
+           
+            
            
         </div>
+       )
     );
 };
 
